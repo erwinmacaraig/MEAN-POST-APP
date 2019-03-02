92bda77224e17c66166f444a52877a17a3217400
Mister M Codes <mister.m.codes@gmail.com>
2019-03-02 15:52:29 +0800

    completed pagination

--- backend/routes/posts.js
+++ backend/routes/posts.js
@@ -53,17 +53,22 @@ router.get('', (req, res, next) => {
   const pageSize = +req.query.pagesize;
   const currentPage = +req.query.page;
   const postQuery = Post.find();
+  let fetchedPosts;
   if (pageSize && currentPage) {
     postQuery
       .skip(pageSize * (currentPage - 1))
       .limit(pageSize);
 
   }
-  Post.find().then(documents => {
-    console.log(documents);
+  postQuery.find().then(documents => {
+    fetchedPosts = documents;
+    return Post.count();
+  })
+  .then(count => {
     res.status(200).json({
       message: 'Posts fetched successfully',
-      posts: documents
+      posts: fetchedPosts,
+      maxPosts: count
     });
   });
   // as a note: find does not really return a promise but rather the objects
