e688ea63aa7a62acd4fa3de056c1d20445188626
Mister M Codes <mister.m.codes@gmail.com>
2019-03-09 19:54:12 +0800

    Lecture 102 - Implementing SPA Authentication

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+
 
 const router = express.Router();
 
@@ -25,6 +27,37 @@ router.post('/signup', (req, res, next) => {
         });
     });
 
+});
+
+router.post('/login', (req, res, next) => {
+  User.findOne({ email: req.body.email}).then(user => {
+    if(!user) {
+      return res.status(401).json({
+        message: 'Auth failed'
+      });
+    }
+    return bcrypt.compare(req.body.password, user.password);
+  })
+  .then(result => {
+    if (!result) {
+      return res.status(401).json({
+        message: 'Auth failed'
+      });
+    }
+    // create a new token based on an input of your choice
+    const token = jwt.sign({
+      email: user.email,
+      userId: user._id
+    }, 'secret_this_should_be_longer',
+    {
+      expiresIn: '1h'
+    });
+  })
+  .catch(err => {
+    return res.status(401).json({
+      message: 'Auth failed'
+    });
+  });
 });
 
 module.exports = router;
