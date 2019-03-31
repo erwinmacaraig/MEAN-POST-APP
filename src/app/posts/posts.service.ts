5ed4ce8c1465d75cd62a3370b51d6f87ee737241
Mister M Codes <mister.m.codes@gmail.com>
2019-03-31 14:15:11 +0800

    Lecture 118 Using the UserId on the Front end

--- src/app/posts/posts.service.ts
+++ src/app/posts/posts.service.ts
@@ -63,7 +63,13 @@ export class PostsService {
 
   getPost(id: string) {
     // return {...this.posts.find(p => p.id === id )};
-    return this.http.get<{_id: string, title: string, content: string, imagePath: string}>('http://localhost:3000/api/posts/' + id);
+    return this.http.get<{
+      _id: string,
+      title: string,
+      content: string,
+      imagePath: string,
+      creator: string
+    }>('http://localhost:3000/api/posts/' + id);
   }
 
   updatePost(id: string, title: string, content: string, image: File | string) {
@@ -80,7 +86,8 @@ export class PostsService {
         id: id,
         title: title,
         content: content,
-        imagePath: image
+        imagePath: image,
+        creator: null
       };
     }
     this.http.put('http://localhost:3000/api/posts/' + id, postData)
